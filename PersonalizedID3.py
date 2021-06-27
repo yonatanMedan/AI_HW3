52d3f1e5a7c4dd000ed9819b05257c207ca40db8
yonatan medan <yonatan.medan@gmail.com>
2021-06-27 14:22:44 +0300

    more improvements

--- PersonalizedID3.py
+++ PersonalizedID3.py
@@ -4,32 +4,58 @@ import numpy as np
 import time
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 
 
 class PersonalizedID3(ID3):
     def __init__(self,data_size=300,par=4.2):
         super().__init__(data_size,par)
         self.pca_transformer=None
-        self.standard_scaler_transformer = None
+        self.standard_scaler_transformer_pca = None
+        self.standard_scaler_transformer_kmeans = None
+        self.kmeans_transformer = None
         
     def addPCAFeatures(self,data,train=True):
         if train==True:
-            self.standard_scaler_transformer = StandardScaler().fit(data)
-            X = self.standard_scaler_transformer.transform(data)
+            self.standard_scaler_transformer_pca = StandardScaler().fit(data)
+            X = self.standard_scaler_transformer_pca.transform(data)
             pca = PCA(n_components=1)
             self.pca_transformer = pca.fit(X)
             principalComponents = self.pca_transformer.transform(X)
         else:
-            X = self.standard_scaler_transformer.transform(data)
+            X = self.standard_scaler_transformer_pca.transform(data)
             principalComponents = self.pca_transformer.transform(X)
         
         return np.concatenate([data,principalComponents],axis=1)
     
+    def add2MeansFeature(self,data,train=True):
+        if train: 
+            kmeans = KMeans(init="random",
+                            n_clusters=2,
+                            n_init=10,
+                            max_iter=300,
+                            random_state=42)
+            self.standard_scaler_transformer_kmeans = StandardScaler().fit(data)
+            X = self.standard_scaler_transformer_kmeans.transform(data)
+            self.kmeans_transformer = kmeans.fit(X)
+
+        else:
+            X = self.standard_scaler_transformer_kmeans.transform(data)
+        
+        kmean_distances_from_centroid = self.kmeans_transformer.transform(X)
+        kmean_predictions = self.kmeans_transformer.predict(X)
+        kmean_features = np.concatenate([kmean_distances_from_centroid,kmean_predictions[np.newaxis].T],axis=1)       
+        return np.concatenate([data,kmean_features],axis=1)
+
+            
+        
+        
+    
     def select_features(self,data):
-        feature_importance = np.array([False, False, False, False,  False, False,  False, False, False,
-       False, False, False,  False,  False,  True, True, True, False,
-        False,  False, False,  True,  True, True, False,  True,  False,
-        True, False, False,  True, False])
+        feature_importance = np.array([True, True, True, True,  True, True,  True, True, True,
+       True, True, False,  False,  True,  True, True, True, False,
+        False,  False, True,  True,  True, True, True,  True,  True,
+        True, True, True,  True])
         data = data[:,feature_importance]
         return data
         
@@ -39,9 +65,9 @@ class PersonalizedID3(ID3):
             Y = data[:,[LABEL_COL]]
         else:
             X = data
-        
+#         X = self.add2MeansFeature(X)
         X = self.addPCAFeatures(X)
-#         X = self.select_features(X)
+        X = self.select_features(X)
         
         if train==True:
             data = np.concatenate([Y,X],axis=1)
@@ -67,7 +93,7 @@ if __name__=="__main__":
     df = pd.read_csv("train.csv",names = ["Y"]+["x_{i}".format(i=i) for i in range(30)])
 #     df = pd.concat([df,principalDf],axis=1)
     data = df.to_numpy()
-    results,losses = train_with_cross_validation(data,PersonalizedID3,4.2)
+    results,losses = train_with_cross_validation(data,PersonalizedID3,3)
     average_accuracy = np.array(results).mean()
     average_spacial_loss = np.array(losses).mean()
     print("Trained ID3 with 5Fold Cross validation average accuracy of {}".format(average_accuracy))
